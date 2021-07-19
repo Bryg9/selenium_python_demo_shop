de4b2f2b9819474017c9660eead5ca0868292068
Brygida Juszczak <bryg9@users.noreply.github.com>
2021-07-19 18:54:37 +0000

    Adding first test with wrong credentials

---
+++ pages/login_page.py
@@ -0,0 +1,40 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+#from pages.sauce_shop_main_page import SaucePage
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from locators import LoginPageLocators
+
+class LoginPage:
+    def __init__(self, driver: WebDriver):
+        self.url = 'https://www.saucedemo.com/'
+        self.driver = driver
+
+    def visit(self):
+        self.driver.get(self.url)
+
+    def log_in_website_check(self):
+        # check if login credentials are on page
+        login_cred = WebDriverWait(self.driver, 3).until(EC.presence_of_element_located(LoginPageLocators.LOGIN_CRED))
+        print('You are on login page.',login_cred.is_displayed())
+
+    def log_in(self, user, password):
+
+       # type login in input box
+       login_input = self.driver.find_element(*LoginPageLocators.LOGIN_INPUT)
+       login_input.click()
+       login_input.send_keys(user)
+
+       # type password in input box
+       password_input = self.driver.find_element(*LoginPageLocators.PASSWORD_INPUT)
+       password_input.click()
+       password_input.send_keys(password)
+
+       # click button 'Login'
+       login_button = self.driver.find_element(*LoginPageLocators.LOGIN_BTN)
+       login_button.click()
+
+       #return SaucePage(self.driver)
+
+    def wrong_credentials_alert(self):
+        alert = self.driver.find_element(*LoginPageLocators.ALERT_CHECK)
+        return alert.text
---
+++ tests/wrong_login_test.py
@@ -0,0 +1,35 @@
+import unittest
+from selenium import webdriver
+from pages.login_page import LoginPage
+import time
+
+
+class WrongCredentialsTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.get('https://www.saucedemo.com/')
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(30)
+
+    def test_wrong_credentials(self):
+        login_page = LoginPage(self.driver)
+
+        # 1. go to page https://www.saucedemo.com/
+        login_page.visit()
+        login_page.log_in_website_check()
+
+        # 2. type incorrect credentials
+        login_page.log_in('any_user', 'secret_pass')
+
+        # 3. check if notification: 'Username and password do not match any user in this service' is displayed
+        messages_text = login_page.wrong_credentials_alert()
+        self.assertEqual('Epic sadface: Username and password do not match any user in this service', messages_text,
+                         f'Expected login button text differ from actual: {messages_text}')
+
+    def tearDown(self):
+        time.sleep(3)
+        self.driver.quit()
+
+
+if __name__=="__main__":
+    unittest.main(verbosity=2)
