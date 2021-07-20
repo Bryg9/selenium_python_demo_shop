bd6908ffeedd5fb1bc44695288bf8597dc4aae19
Brygida Juszczak <bryg9@users.noreply.github.com>
2021-07-20 09:48:37 +0000

    Correcting test with wrong credentials

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,8 +1,9 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
-#from pages.sauce_shop_main_page import SaucePage
+from pages.sauce_shop_main_page import SaucePage
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators import LoginPageLocators
+
 
 class LoginPage:
     def __init__(self, driver: WebDriver):
@@ -14,26 +15,26 @@ class LoginPage:
 
     def log_in_website_check(self):
         # check if login credentials are on page
-        login_cred = WebDriverWait(self.driver, 3).until(EC.presence_of_element_located(LoginPageLocators.LOGIN_CRED))
-        print('You are on login page.',login_cred.is_displayed())
+        login_cred = WebDriverWait(self.driver, 3).until(EC.presence_of_element_located(LoginPageLocators.LOGIN_CRED))# noqa
+        print('You are on login page.', login_cred.is_displayed())
 
     def log_in(self, user, password):
 
-       # type login in input box
-       login_input = self.driver.find_element(*LoginPageLocators.LOGIN_INPUT)
-       login_input.click()
-       login_input.send_keys(user)
+        # type login in input box
+        login_input = self.driver.find_element(*LoginPageLocators.LOGIN_INPUT)
+        login_input.click()
+        login_input.send_keys(user)
 
-       # type password in input box
-       password_input = self.driver.find_element(*LoginPageLocators.PASSWORD_INPUT)
-       password_input.click()
-       password_input.send_keys(password)
+        # type password in input box
+        password_input = self.driver.find_element(*LoginPageLocators.PASSWORD_INPUT)# noqa
+        password_input.click()
+        password_input.send_keys(password)
 
-       # click button 'Login'
-       login_button = self.driver.find_element(*LoginPageLocators.LOGIN_BTN)
-       login_button.click()
+        # click button 'Login'
+        login_button = self.driver.find_element(*LoginPageLocators.LOGIN_BTN)
+        login_button.click()
 
-       #return SaucePage(self.driver)
+        return SaucePage(self.driver)
 
     def wrong_credentials_alert(self):
         alert = self.driver.find_element(*LoginPageLocators.ALERT_CHECK)
--- tests/wrong_login_test.py
+++ tests/wrong_login_test.py
@@ -21,15 +21,15 @@ class WrongCredentialsTest(unittest.TestCase):
         # 2. type incorrect credentials
         login_page.log_in('any_user', 'secret_pass')
 
-        # 3. check if notification: 'Username and password do not match any user in this service' is displayed
+        # 3. check if notification: 'Username and password do not match any user in this service' is displayed# noqa
         messages_text = login_page.wrong_credentials_alert()
-        self.assertEqual('Epic sadface: Username and password do not match any user in this service', messages_text,
-                         f'Expected login button text differ from actual: {messages_text}')
+        self.assertEqual('Epic sadface: Username and password do not match any user in this service', messages_text,# noqa
+                         f'Expected login button text differ from actual: {messages_text}')# noqa
 
     def tearDown(self):
         time.sleep(3)
         self.driver.quit()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     unittest.main(verbosity=2)
